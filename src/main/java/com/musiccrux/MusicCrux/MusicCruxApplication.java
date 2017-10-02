0f452cf435d87d7fe1461d3e7bcfad12d1cc29d5
Taiwo Oyekanmi <toyekanmi@localhost.localdomain>
2017-10-02 10:59:14 -0400

    Starter Project

---
+++ src/main/java/com/musiccrux/MusicCrux/MusicCruxApplication.java
@@ -0,0 +1,12 @@
+package com.musiccrux.MusicCrux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCruxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCruxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/musiccrux/MusicCrux/MusicCruxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.musiccrux.MusicCrux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicCruxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
