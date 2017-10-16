a91a7b7acb428d188596446988c398c36ddd0774
Ati Ok <ao10@umbc.edu>
2017-10-16 13:16:56 -0400

    Removed unused Artist class & import

--- Music-Crux-App/src/main/java/application/server/controllers/ArtistController.java
+++ Music-Crux-App/src/main/java/application/server/controllers/ArtistController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import application.dummies.Artist;
 import application.dummies.Entity;
 import application.dummies.EntityType;
 import application.dummies.Member;
