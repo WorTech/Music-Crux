b99c69ce79bc2b191a7d4aa74792f66e0d5c463c
Taiwo Oyekanmi <tooyeka@HeavySeas.wtc.md>
2017-11-03 17:27:50 -0400

    Added endpoints to the entity controller

--- Music-Crux-App/src/main/java/application/api/controllers/EntityController.java
+++ Music-Crux-App/src/main/java/application/api/controllers/EntityController.java
@@ -4,25 +4,40 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import application.api.models.Molecule;
 import application.api.models.db.Entity;
 import application.api.service.EntityService;
 
 @RestController
-@RequestMapping("/api/entities")
+@RequestMapping("/api")
 public class EntityController {
-	
+
 	@Autowired
 	EntityService entityService;
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<Entity>> getEntities() {
-		
-		List<Entity> entities = entityService.getEntities();
-		return new ResponseEntity<List<Entity>>(entities, HttpStatus.OK);
+
+	@RequestMapping(value = "/entity/{depth}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Molecule> createMoleculeFromEntity(@RequestBody Entity entity,
+			@PathVariable("depth") Integer depth) {
+
+		Molecule molecule = entityService.createMoleculeFromEntity(entity, depth);
+		HttpStatus status = molecule == null ? HttpStatus.NO_CONTENT : HttpStatus.OK;
+		return new ResponseEntity<Molecule>(molecule, status);
+	}
+
+	@RequestMapping(value = "/entities/{depth}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Molecule> createMoleculeFromEntities(@RequestBody List<Entity> entities,
+			@PathVariable("depth") Integer depth) {
+
+		Molecule molecule = entityService.createMoleculeFromEntities(entities, depth);
+		HttpStatus status = molecule == null ? HttpStatus.NO_CONTENT : HttpStatus.OK;
+		return new ResponseEntity<Molecule>(molecule, status);
 	}
 }
