586fe7f90773d1c959539f96c3cd7dc8db166a38
Taiwo Oyekanmi <tooyeka@HeavySeas.wtc.md>
2017-11-27 12:06:54 -0500

    Added relationship controller and query for relationships

---
+++ Music-Crux-App/src/main/java/application/api/controllers/RelationshipController.java
@@ -0,0 +1,35 @@
+package application.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.api.models.db.Entity;
+import application.api.models.db.Relationship;
+import application.api.models.ui.RelationshipUI;
+import application.api.repositories.EntityRepository;
+import application.api.repositories.RelationshipRepository;
+import application.api.service.RelationshipService;
+
+@RestController
+@RequestMapping("/api")
+public class RelationshipController {
+	
+	@Autowired
+	RelationshipService relationshipService;
+	
+	@RequestMapping(value = "/relationship", method = RequestMethod.GET)
+	public ResponseEntity<List<RelationshipUI>> getRelationshipsContainingLabel(@RequestParam("label") String label){
+		
+		List<RelationshipUI> relationships = relationshipService.findByLabel(label);
+		
+		HttpStatus status = relationships == null ? HttpStatus.NO_CONTENT : HttpStatus.OK;
+		return new ResponseEntity<List<RelationshipUI>>(relationships, status);
+	}
+}
--- Music-Crux-App/src/main/java/application/api/repositories/RelationshipRepository.java
+++ Music-Crux-App/src/main/java/application/api/repositories/RelationshipRepository.java
@@ -3,6 +3,7 @@ package application.api.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import application.api.models.RelationshipType;
 import application.api.models.db.Entity;
@@ -12,6 +13,7 @@ import application.api.models.db.Relationship;
 public interface RelationshipRepository extends MongoRepository<Relationship, String>{
 	
 	public Relationship findByType(RelationshipType relationshipType);
-
-	public List<Relationship> findBySourceEntity(Entity sourceEntity);
+	
+	@Query("{$or: [{'entity1' :{'$ref' : 'entity' , '$id' : ?0}}, {'entity2' :{'$ref' : 'entity' , '$id' : ?0}}] }")
+	public List<Relationship> findByEntity(String entity1Id);
 }
